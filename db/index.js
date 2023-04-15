d9eeb041f047456bb98127d961a5604a475adb57
Konstantinos Katris <93059645+katrhss@users.noreply.github.com>
2023-04-15 20:34:14 +0300

    Changed database location

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://admin:admin@local.tol4uo2.mongodb.net/test")
+  .connect("mongodb+srv://admin:admin@local.tol4uo2.mongodb.net/Smart-Kozani")
   .then(() => console.log("db connected"))
   .catch((err) => console.log("db connection failed: " + err.message || err));
